fbb493c1c771d558421f929d0621f5e1ab71b4c0
cy-rus404 <sduisaac@yahoo.com>
2025-07-11 01:56:01 +0000

    initial commit

---
+++ LoginScreen.js
@@ -0,0 +1,31 @@
+import { Alert, Platform, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+
+export default function LoginScreen() {
+
+const handleLogin = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        if (email === "sduisaac@gmail.com" && password === '1234') {
+            Alert.alert("Success", "Logged In");
+
+        } else {
+            Alert.alert('Error', 'Invalid Credentials');
+        }
+    }
+};
+
+const handleSocialLogin = (platform) => {
+    Alert.alert('Social Login', `Logging in with ${platform}`);
+}
+
+  return (
+    <View>
+      <Text>LoginScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
